13d2d2cde04663772715e6ccf7d4625dbd84a527
clayandemar <clayandemar@gmail.com>
2023-01-23 19:59:33 -0600

    Fixed JS
    Website works on first try now

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
 
   // recents opened
@@ -11,9 +10,9 @@ $(document).ready(function () {
   var lsArray = JSON.parse(localStorage.getItem("Searched Cities"));
   if (!lsArray)
     lsArray = [];
+  lsArray.sort();
 
   rebuildMenu();
-
 
   function rebuildMenu() {
     $(".dropdown-menu").empty()
@@ -21,7 +20,6 @@ $(document).ready(function () {
       $(".dropdown-menu").append("<li><a class='dropdown-item' href='#'>" + lsArray[i] + "</a></li>")
     }
     $(".dropdown-item").click(function buttonClick(e) {
-      console.log($(this).text())
       $(".form-control").val($(this).text());
       $(".btn").click();
       $(".dropdown-menu").slideUp();
@@ -29,60 +27,15 @@ $(document).ready(function () {
     });
   }
 
-  // recents click on name of city function
-  // $(".dropdown-item").click(function buttonClick(e) {
-  //   console.log($(this).text())
-  //   $(".form-control").val($(this).text());
-  //   $(".btn").click();
-  //   $(".dropdown-menu").slideUp();
-  //   open = false;
-  // });
-
   //button click for search
   $(".btn").click(function buttonClick(e) {
     var citySearched = $(".form-control").val();
-    $("#allBoxes").fadeIn();
-
-
-
-
-    //  const apiCity = fetch("https://api.openweathermap.org/data/2.5/weather?q=" + citySearched + "&APPID=3dbd7567d6829a773ae96978861ebc2a");
-    var fetchTest = fetch("https://api.openweathermap.org/data/2.5/weather?q=" + citySearched + "&APPID=" + apikey)
-
-      // .then
-      .then(response => response.json())
-      .then(data => {
-
-        //JSON stringify set item
-        if (lsArray.indexOf(citySearched) === -1)
-          lsArray.push(citySearched);
-        localStorage.setItem("Searched Cities", JSON.stringify(lsArray));
-        console.log(lsArray);
-
-
-        //todays weather
-        var farenheit = ((data.main.temp - 273.15) * 9 / 5 + 32)
-
-        $("#cityName").text(data.name + " - " + new Date().toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" }));
-        $("#temp").text("Temp: " + Math.round(farenheit) + "°F");
-        $("#wind").text("Wind: " + data.wind.speed + "mph");
-        $("#humidity").text("Humidity: " + data.main.humidity + "%");
-        // next days
-        fetchTest = fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + citySearched + "&APPID=" + apikey)
-          .then(response => response.json())
-          .then(data => {
-            console.log(data);
-            for (var i = 0; i < 5; i++) {
-              var farenheit2 = ((data.list[i * 8].main.temp - 273.15) * 9 / 5 + 32);
-              $("#cityName" + i).text(new Date(data.list[i * 8].dt * 1000).toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" }));
-              $("#temp" + i).text("Temp: " + Math.round(farenheit2) + "°F");
-              $("#wind" + i).text("Wind: " + data.list[i * 8].wind.speed + "mph");
-              $("#humidity" + i).text("Humidity: " + data.list[i * 8].main.humidity + "%");
-            }
-            rebuildMenu();
-          });
-      });
+    console.log(citySearched);
+    if (citySearched)
+      getResponse();
+    return false;
   });
+
   // recents dropdown
   $(".nav-link").click(function buttonClick(e) {
     if (open === false) {
@@ -92,7 +45,59 @@ $(document).ready(function () {
       $(".dropdown-menu").slideUp();
       open = false;
     }
-
   });
 
+ 
+
+  async function getResponse() {
+    var citySearched = $(".form-control").val();
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearched + "&APPID=" + apikey;
+    const response = await fetch(url, { method: 'GET' });
+    if (!response.ok) {
+      $("#allBoxes").hide();
+      alert("City not found. Try again");
+    }
+    else {
+      const data = await response.json(); 
+      
+      // Extracting data as a JSON Object from the response
+      if (!data) {
+        alert("No results found");
+      }
+      else {
+        console.log(data);
+
+        // City found
+        $("#allBoxes").fadeIn();
+
+        // Add to lsArray if not already there and store in local storage
+        if (lsArray.indexOf(citySearched) === -1)
+          lsArray.push(citySearched);
+        lsArray.sort(); // sort it
+        localStorage.setItem("Searched Cities", JSON.stringify(lsArray));
+
+        // City name and today's weather
+        $("#cityName").text(data.name + " - " + new Date().toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" }));
+        var farenheit = ((data.main.temp - 273.15) * 9 / 5 + 32)
+        $("#temp").text("Temp: " + Math.round(farenheit) + "°F");
+        $("#wind").text("Wind: " + data.wind.speed + "mph");
+        $("#humidity").text("Humidity: " + data.main.humidity + "%");
+
+        // Next 5 days
+        var fetchTest = fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + citySearched + "&APPID=" + apikey)
+          .then(response => response.json())
+          .then(data => {
+            for (var i = 0; i < 5; i++) {
+              var farenheit2 = ((data.list[i * 8].main.temp - 273.15) * 9 / 5 + 32);
+              $("#cityName" + i).text(new Date(data.list[i * 8].dt * 1000).toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" }));
+              $("#temp" + i).text("Temp: " + Math.round(farenheit2) + "°F");
+              $("#wind" + i).text("Wind: " + data.list[i * 8].wind.speed + "mph");
+              $("#humidity" + i).text("Humidity: " + data.list[i * 8].main.humidity + "%");
+            }
+            rebuildMenu();
+          });
+          
+      }
+    }
+  }
 });
